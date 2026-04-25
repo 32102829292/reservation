f805478cfeb261e984783793fe6d2c8e547343e1
32102829292 <gabuatangelimae@gmail.>
2026-04-25 14:17:39 +0800

    New

--- app/Views/landing.php
+++ app/Views/landing.php
@@ -7,6 +7,7 @@
     <meta name="theme-color" content="#1e1b4b">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <?php include(APPPATH . 'Views/partials/head_meta.php'); ?>
     <style>
         *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; -webkit-tap-highlight-color: transparent; }
         :root {
